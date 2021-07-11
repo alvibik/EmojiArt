46e5a34af4b85c6b18e2aedb56e156f2e5013458
Alexander Bikteev <bikteev@mail.ru>
2021-07-11 17:56:32 +0900

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Александр Биктеев on 11.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
